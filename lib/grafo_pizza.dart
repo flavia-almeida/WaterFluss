dcc46d72a8ff0073be878b491d433599bd767fb3
flavia-almeida <flaviamelo13@hotmail.com>
2022-05-30 14:03:47 -0300

    pizza funcionando

--- lib/grafo_pizza.dart
+++ lib/grafo_pizza.dart
@@ -8,6 +8,13 @@ class Pizza extends StatefulWidget {
   State<Pizza> createState() => _PizzaState();
 }
 
+class Data {
+  final String nome;
+  final double porcent;
+  final Color color;
+  Data({required this.nome, required this.porcent, required this.color});
+}
+
 class _PizzaState extends State<Pizza> {
   double valor = 60.0;
   @override
@@ -15,8 +22,7 @@ class _PizzaState extends State<Pizza> {
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.only(top:50),
-          
+        padding: const EdgeInsets.only(top: 50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[Grafico()],
@@ -25,49 +31,66 @@ class _PizzaState extends State<Pizza> {
     );
   }
 
-  set_dados(){
+  set_dados() {
     return List.generate(1, (index) {
       final fontSize = 14.0;
       final Radius = 0.0;
       final color = Color.fromARGB(255, 56, 72, 214);
 
       return PieChartSectionData(
-                color: Color.fromARGB(255, 56, 72, 214),
-                value: valor * 100,
-                title: '${valor.toStringAsFixed(0)}%',
-                radius: 110.0
-              );
+          color: Color.fromARGB(255, 56, 72, 214),
+          value: valor * 100,
+          title: '${valor.toStringAsFixed(0)}%',
+          radius: 110.0);
     });
   }
 
-  Grafico(){
+  List<Data> data = [
+    Data(
+        nome: 'contem',
+        porcent: 15000,
+        color: Color.fromRGBO(102, 102, 102, 1.0)),
+    Data(
+        nome: 'vazio',
+        porcent: 5000,
+        color: Color.fromRGBO(255, 255, 255, 1.0)),
+  ];
+
+  List<PieChartSectionData> getSections() => data
+      .asMap()
+      .map<int, PieChartSectionData>((index, data) {
+        final value = PieChartSectionData(
+            color: data.color, value: data.porcent, title: '${data.porcent}%');
+        return MapEntry(index, value);
+      })
+      .values
+      .toList();
+
+  Grafico() {
     if ((valor <= 0)) {
       return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 200,
-      child: const Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+        width: MediaQuery.of(context).size.width,
+        height: 200,
+        child: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
     } else {
       return Stack(
-      alignment: Alignment.center,
-      children: [
-        AspectRatio(
-          aspectRatio: 1,
-          child: PieChart(
-            PieChartData(
-              sectionsSpace: 5,
-              centerSpaceRadius: 50,
-              sections: set_dados(),
+        alignment: Alignment.center,
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: PieChart(
+              PieChartData(
+                sectionsSpace: 5,
+                centerSpaceRadius: 0,
+                sections: getSections(),
+              ),
             ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
     }
   }
 }
-
-
-
